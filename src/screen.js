4670cde435b1d1bf2d5c637b4c3ec522845ae08a
Richard <richard.anaya@gmail.com>
2013-09-20 02:32:44 -0500

    adding bloom effect

--- src/screen.js
+++ src/screen.js
@@ -1,132 +1,116 @@
 var Screen = function(){
-    var container, stats;
+    $(document.body).append('<div id="container""></div>');
+
+    container = document.getElementById( 'container' );
+    $(container).css("position","absolute");
+    $(container).css("top","0px");
+    $(container).css("left","0px");
+    $(container).css("margin","0px");
+    $(container).css("padding","0px");
+    $(document.body).css("overflow","hidden");
+    $(document.body).css("background-color","black");
+
+
+
+    var container;
 
     var camera, scene, renderer;
 
-    var mesh;
+    var video, texture, material, mesh;
+
+    var composer;
+
+    var mouseX = 0;
+    var mouseY = 0;
+
+    var windowHalfX = window.innerWidth / 2;
+    var windowHalfY = window.innerHeight / 2;
+
+    var cube_count,
+
+        meshes = [],
+        materials = [],
+
+        xgrid = 20,
+        ygrid = 10;
+
+    init();
+    animate();
 
     function init() {
-        $(document.body).append('<div id="container""></div>');
 
-        container = document.getElementById( 'container' );
-        $(container).css("position","absolute");
-        $(container).css("top","0px");
-        $(container).css("left","0px");
-        $(container).css("margin","0px");
-        $(container).css("padding","0px");
-        $(document.body).css("overflow","hidden");
-        $(document.body).css("background-color","black");
+        container = document.createElement( 'div' );
+        document.body.appendChild( container );
 
-        //
-
-        camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 4000 );
-        camera.position.z = 2750;
+        camera = new THREE.OrthographicCamera( 0, window.innerWidth, window.innerHeight, 0, - 500, 1000 );
+        camera.position.z = 500;
 
         scene = new THREE.Scene();
 
+        var light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 0.5, 1, 1 ).normalize();
+        scene.add( light );
 
-        var segments = 10000;
-
-        var geometry = new THREE.BufferGeometry();
-        var material = new THREE.LineBasicMaterial({ vertexColors: true });
-
-        geometry.attributes = {
-            position: {
-                itemSize: 3,
-                array: new Float32Array(segments * 3)
-            },
-            color: {
-                itemSize: 3,
-                array: new Float32Array(segments * 3)
-            }
-        };
-
-        var positions = geometry.attributes.position.array;
-        var colors = geometry.attributes.color.array;
-
-        var r = 800;
-
-        for ( var i = 0; i < segments; i ++ ) {
-
-            var x = Math.random() * r - r / 2;
-            var y = Math.random() * r - r / 2;
-            var z = Math.random() * r - r / 2;
-
-            // positions
-
-            positions[ i * 3 ] = x;
-            positions[ i * 3 + 1 ] = y;
-            positions[ i * 3 + 2 ] = z;
-
-            // colors
-
-            colors[ i * 3 ] = ( x / r ) + 0.5;
-            colors[ i * 3 + 1 ] = ( y / r ) + 0.5;
-            colors[ i * 3 + 2 ] = ( z / r ) + 0.5;
-
-        }
-
-        geometry.computeBoundingSphere();
-
-        mesh = new THREE.Line( geometry, material );
-        scene.add( mesh );
-
-        //
-
-        renderer = new THREE.WebGLRenderer( { antialias: false, alpha: false } );
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
         renderer.setSize( window.innerWidth, window.innerHeight );
-
-        renderer.gammaInput = true;
-        renderer.gammaOutput = true;
-        renderer.physicallyBasedShading = true;
 
         container.appendChild( renderer.domElement );
 
-        //
+        // Cubes
+        var geometry = new THREE.CubeGeometry( 50, 50, 50 );
+        var material = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, overdraw: 0.5 } );
 
-        stats = new Stats();
-        stats.domElement.style.position = 'absolute';
-        stats.domElement.style.top = '0px';
-        container.appendChild( stats.domElement );
+        for(var x = 0; x < 8; x++){
+            for(var y = 0; y < 8; y++){
+                var cube = new THREE.Mesh( geometry, material );
 
-        //
+                cube.position.x = (window.innerWidth-8*55)/2+25+x*55;
+                cube.position.y = (window.innerHeight-8*55)/2+35+y*55;
+                cube.position.z = 100;
+
+                scene.add( cube );
+            }
+        }
+
+        renderer.autoClear = false;
+
+
+        // postprocessing
+
+        var renderModel = new THREE.RenderPass( scene, camera );
+        var effectBloom = new THREE.BloomPass( 1.3 );
+        var effectCopy = new THREE.ShaderPass( THREE.CopyShader );
+
+        effectCopy.renderToScreen = true;
+
+        composer = new THREE.EffectComposer( renderer );
+
+        composer.addPass( renderModel );
+        composer.addPass( effectBloom );
+        composer.addPass( effectCopy );
 
         window.addEventListener( 'resize', onWindowResize, false );
-
     }
 
     function onWindowResize() {
+        windowHalfX = window.innerWidth / 2;
+        windowHalfY = window.innerHeight / 2;
 
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
 
         renderer.setSize( window.innerWidth, window.innerHeight );
-
+        composer.reset();
     }
 
-//
 
     function animate() {
-
         requestAnimationFrame( animate );
-
         render();
-        stats.update();
-
     }
 
     function render() {
-
-        var time = Date.now() * 0.001;
-
-        mesh.rotation.x = time * 0.25;
-        mesh.rotation.y = time * 0.5;
-
-        renderer.render( scene, camera );
-
+        renderer.clear();
+        composer.render();
     }
-
-
-    init();
-    animate();
 }
